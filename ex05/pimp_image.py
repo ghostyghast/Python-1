ac422de1d3ec8686e00a2abb78aa1c329f3a6598
ghostyghast <antoinethebestgamer@gmail.com>
2023-09-30 17:22:43 +0800

    ex05 almost finshed, still figuring out how to turn to greyscale, rest is complete

--- ex05/pimp_image.py
+++ ex05/pimp_image.py
@@ -3,22 +3,32 @@ import numpy as np
 
 def ft_invert(array):
     """inverts the color of the image received"""
-    return np.subtract(255, array[:, :])
+    return np.subtract[255, array[:, :]]
 
 
 def ft_red(array):
     """turns the given image red"""
-    n_array = array
-    n_array[:, :] = [255, 0, 0]
-    return n_array
+    return array[:, :] * [1, 0, 0]
+
 
 def ft_green(array):
     """turns the given image green"""
+    n_array = array.copy()
+    n_array[:, :, 0] = 0
+    n_array[:, :, 2] = 0
+    return n_array
 
 
 def ft_blue(array):
     """turns the given image blue"""
+    n_array = array.copy()
+    n_array[:, :, 0] = 0
+    n_array[:, :, 1] = 0
+    return n_array
 
 
 def ft_grey(array):
     """turns the given image grey"""
+    n_array = array.copy()
+    n_array[:, :] = (np.sum[:, :] / 3)
+    return n_array
